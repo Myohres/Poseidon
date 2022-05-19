de2b65c9c1556e71797115cd8be5138c81719de6
Myohres <77498044+Myohres@users.noreply.github.com>
2022-05-19 06:23:53 +0200

    domain RuleNameEntity

---
+++ src/main/java/com/nnk/poseidon/domain/RuleNameEntity.java
@@ -0,0 +1,24 @@
+package com.nnk.poseidon.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "rulename")
+public class RuleNameEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String description;
+    private String json;
+    private String template;
+    private String sqlStr;
+    private String sqlPart;
+
+}
