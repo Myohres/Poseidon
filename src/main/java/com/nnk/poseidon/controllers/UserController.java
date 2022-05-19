d1acda29afec3b17d0f7f9d3bba44d182a0a7fc0
Myohres <77498044+Myohres@users.noreply.github.com>
2022-05-19 07:10:14 +0200

    controller starter class

---
+++ src/main/java/com/nnk/poseidon/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.nnk.poseidon.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/user")
+@CrossOrigin("http://localhost:4200")
+public class UserController {
+}
---
+++ src/main/java/com/nnk/poseidon/controllers/ruleNameController.java
@@ -0,0 +1,13 @@
+package com.nnk.poseidon.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/ruleName")
+@CrossOrigin("http://localhost:4200")
+public class ruleNameController {
+}
