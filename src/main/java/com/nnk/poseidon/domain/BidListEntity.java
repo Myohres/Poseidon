d273d50cd995c21bcddef8f0a9db40bc261b20e9
Myohres <77498044+Myohres@users.noreply.github.com>
2022-05-19 06:07:16 +0200

    domain BidListEntity

---
+++ src/main/java/com/nnk/poseidon/domain/BidListEntity.java
@@ -0,0 +1,36 @@
+package com.nnk.poseidon.domain;
+
+import org.apache.tomcat.jni.Time;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "bidlist")
+public class BidListEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int bidListId;
+    private  String account;
+    private String type;
+    private double bidQuantity;
+    private double askQuantity;
+    private double bid;
+    private double ask;
+    private String benchmark;
+    private Timestamp bidListDate;
+    private String commentary;
+    private String security;
+    private String status;
+    private String trader;
+    private String book;
+    private String creationName;
+    private Timestamp creationDate;
+    private String revisionName;
+    private Timestamp revisionDate;
+    private String dealName;
+    private String dealType;
+    private String sourceListId;
+    private String side;
+}
