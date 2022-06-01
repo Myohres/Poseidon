c532c0aa00b4716b43da8ea4ef865ff4c88c2c74
Myohres <77498044+Myohres@users.noreply.github.com>
2022-06-01 22:27:21 +0200

    curvePoint service CRUD ok

---
+++ src/main/java/com/nnk/poseidon/service/CurvePointService.java
@@ -0,0 +1,66 @@
+package com.nnk.poseidon.service;
+
+import com.nnk.poseidon.domain.CurvePointEntity;
+import com.nnk.poseidon.repositories.CurvePointRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class CurvePointService {
+
+    @Autowired
+    private CurvePointRepository curvePointRepository;
+
+    /**
+     * Find all CurvePoint.
+     * @return List of all CurvePoint
+     */
+    public List<CurvePointEntity> findAll() {
+        return curvePointRepository.findAll();
+    }
+
+    /**
+     * Find curePoint by id.
+     * @param id Curve id
+     * @return CurvePoint
+     */
+    public CurvePointEntity findById(final Integer id) {
+        return curvePointRepository.findById(id).orElseThrow(()
+                -> new NoSuchElementException(
+                        "No CurvePoint found with id : " + id));
+    }
+
+    /**
+     * Save curvePoint.
+     * @param curvePoint CurvePointEntity
+     * @return curvePoint saved
+     */
+    public CurvePointEntity add(final CurvePointEntity curvePoint) {
+        return curvePointRepository.save(curvePoint);
+    }
+
+    /**
+     * Update curvePointInfo.
+     * @param newCurvePoint new curvePoint infos
+     * @return curvePoint updated
+     */
+    public CurvePointEntity update(final CurvePointEntity newCurvePoint) {
+        CurvePointEntity oldCurvePoint = findById(newCurvePoint.getCurveId());
+        oldCurvePoint.setCurveId(newCurvePoint.getCurveId());
+        oldCurvePoint.setTerm(newCurvePoint.getTerm());
+        oldCurvePoint.setValue(newCurvePoint.getValue());
+        return curvePointRepository.save(oldCurvePoint);
+    }
+
+    /**
+     * Delete curvePoint by id.
+     * @param id curveId
+     */
+    public void delete(final Integer id) {
+        CurvePointEntity curvePointEntity = findById(id);
+        curvePointRepository.delete(curvePointEntity);
+    }
+}
