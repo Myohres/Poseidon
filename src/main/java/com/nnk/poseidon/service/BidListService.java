cf605d270e297a3b8b877b7df42ecf12fbe3ee92
Myohres <77498044+Myohres@users.noreply.github.com>
2022-06-01 16:20:33 +0200

    service bidListService reduce Update

--- src/main/java/com/nnk/poseidon/service/BidListService.java
+++ src/main/java/com/nnk/poseidon/service/BidListService.java
@@ -32,24 +32,6 @@ public class BidListService {
         oldBidListEntity.setAccount(newBidListEntity.getAccount());
         oldBidListEntity.setType(newBidListEntity.getType());
         oldBidListEntity.setBidQuantity(newBidListEntity.getBidQuantity());
-        oldBidListEntity.setAskQuantity(newBidListEntity.getAskQuantity());
-        oldBidListEntity.setBid(newBidListEntity.getBid());
-        oldBidListEntity.setAsk(newBidListEntity.getAsk());
-        oldBidListEntity.setBenchmark(newBidListEntity.getBenchmark());
-        oldBidListEntity.setBidListDate(newBidListEntity.getBidListDate());
-        oldBidListEntity.setCommentary(newBidListEntity.getCommentary());
-        oldBidListEntity.setSecurity(newBidListEntity.getSecurity());
-        oldBidListEntity.setStatus(newBidListEntity.getStatus());
-        oldBidListEntity.setTrader(newBidListEntity.getTrader());
-        oldBidListEntity.setBook(newBidListEntity.getBook());
-        oldBidListEntity.setCreationName(newBidListEntity.getCreationName());
-        oldBidListEntity.setCreationDate(newBidListEntity.getCreationDate());
-        oldBidListEntity.setRevisionName(newBidListEntity.getRevisionName());
-        oldBidListEntity.setRevisionDate(newBidListEntity.getRevisionDate());
-        oldBidListEntity.setDealName(newBidListEntity.getDealName());
-        oldBidListEntity.setDealType(newBidListEntity.getDealType());
-        oldBidListEntity.setSourceListId(newBidListEntity.getSourceListId());
-        oldBidListEntity.setSide(newBidListEntity.getSide());
         return bidListRepository.save(oldBidListEntity);
     }
 
