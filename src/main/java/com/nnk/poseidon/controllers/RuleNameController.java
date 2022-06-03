6dc1007e0d6bd1632d96fc9c1e79800719905117
Myohres <77498044+Myohres@users.noreply.github.com>
2022-06-03 02:09:11 +0200

    ruleName controller still validate

--- src/main/java/com/nnk/poseidon/controllers/RuleNameController.java
+++ src/main/java/com/nnk/poseidon/controllers/RuleNameController.java
@@ -1,7 +1,9 @@
 package com.nnk.poseidon.controllers;
 
+import com.nnk.poseidon.domain.RatingEntity;
 import com.nnk.poseidon.domain.RuleNameEntity;
-import lombok.extern.slf4j.Slf4j;
+import com.nnk.poseidon.service.RuleNameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,47 +14,106 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
 @CrossOrigin("http://localhost:4200")
 public class RuleNameController {
-    // TODO: Inject RuleName service
 
+    /**
+     * RuleName Service layer.
+     */
+    @Autowired
+    private RuleNameService ruleNameService;
+
+    /**
+     * Get ruleName list page.
+     * @param model rating list to pass
+     * @return ruleName list page
+     */
     @RequestMapping("/ruleName/list")
-    public String home(Model model)
+    public String home(final Model model)
     {
-        // TODO: find all RuleName, add to model
+        List<RuleNameEntity> list = ruleNameService.findAll();
+        model.addAttribute("list", list);
         return "ruleName/list";
     }
 
+    /**
+     * Get adding ruleName page.
+     * @param ruleName ruleName to add
+     * @param model ruleName to pass
+     * @return adding page
+     */
     @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleNameEntity bid) {
+    public String addRuleForm(RuleNameEntity ruleName, final Model model) {
+        model.addAttribute("ruleName", ruleName);
         return "ruleName/add";
     }
 
+    /**
+     * Validate ruleName for adding.
+     * @param ruleName ratting to add
+     * @param result binding
+     * @param model ruleName list
+     * @return ruleName list page
+     */
     @PostMapping("/ruleName/validate")
-    public String validate(@Valid RuleNameEntity ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
+    public String validate(@Valid final RuleNameEntity ruleName, final BindingResult result, final Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("ruleName", ruleName);
+            //TODO display errors
         return "ruleName/add";
+        }
+            ruleNameService.add(ruleName);
+            model.addAttribute("list", ruleNameService.findAll());
+            return "ruleName/add";
     }
 
+    /**
+     * Get updating ruleName page.
+     * @param id ruleName id
+     * @param model ruleName to pass
+     * @return ruleName update page
+     */
     @GetMapping("/ruleName/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
+    public String showUpdateForm(@PathVariable("id") final Integer id, final Model model) {
+        RuleNameEntity ruleNameEntity = ruleNameService.findById(id);
+        model.addAttribute("ruleName", ruleNameEntity);
         return "ruleName/update";
     }
 
+    /**
+     * Update ruleName.
+     * @param id ruleName id
+     * @param ruleName ruleName to update
+     * @param result binding
+     * @param model ruleName list to pass
+     * @return ruleName list page if ok else ruleName update page
+     */
     @PostMapping("/ruleName/update/{id}")
-    public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleNameEntity ruleName,
-                                 BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
+    public String updateRuleName(@PathVariable("id") final Integer id, @Valid final RuleNameEntity ruleName,
+                                 final BindingResult result, final Model model) {
+        if (result.hasErrors()) {
+            return "redirect:/ruleName/list";
+        }
+        ruleName.setId(id);
+        ruleNameService.update(ruleName);
+        model.addAttribute("list", ruleNameService.findAll());
         return "redirect:/ruleName/list";
     }
 
+    /**
+     * Delete ratting.
+     * @param id ratting id
+     * @param model ratting list
+     * @return ratting list page
+     */
     @GetMapping("/ruleName/delete/{id}")
-    public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
+    public String deleteRuleName(@PathVariable("id") final Integer id, final Model model) {
+        ruleNameService.delete(id);
+        model.addAttribute("list", ruleNameService.findAll());
         return "redirect:/ruleName/list";
     }
 }
