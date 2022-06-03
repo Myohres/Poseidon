b1f930bd0815d9faa51e3a276821d1f5fef6c861
Myohres <77498044+Myohres@users.noreply.github.com>
2022-06-03 04:19:32 +0200

    trade controller ok still validate

--- src/main/java/com/nnk/poseidon/controllers/TradeController.java
+++ src/main/java/com/nnk/poseidon/controllers/TradeController.java
@@ -1,7 +1,8 @@
 package com.nnk.poseidon.controllers;
 
 import com.nnk.poseidon.domain.TradeEntity;
-import lombok.extern.slf4j.Slf4j;
+import com.nnk.poseidon.service.TradeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,42 +18,103 @@ import javax.validation.Valid;
 
 @CrossOrigin("http://localhost:4200")
 public class TradeController {
-    // TODO: Inject Trade service
 
+    /**
+     * Trade service layer.
+     */
+    @Autowired
+    private TradeService tradeService;
+
+    /**
+     * Show all Trade.
+     * @param model list Trade
+     * @return Trade list page
+     */
     @RequestMapping("/trade/list")
-    public String home(Model model)
-    {
-        // TODO: find all Trade, add to model
+    public String home(final Model model) {
+        model.addAttribute("list", tradeService.findAll());
         return "trade/list";
     }
 
+    /**
+     * Go to adding Trade page.
+     * @param trade Trade to add
+     * @param model Trade to pass
+     * @return Trade adding page
+     */
     @GetMapping("/trade/add")
-    public String addUser(TradeEntity bid) {
+    public String addUser(final TradeEntity trade, final Model model) {
+
+        model.addAttribute("trade", trade);
         return "trade/add";
     }
 
+    /**
+     * Validate fields Trade before adding.
+     * @param trade Trade to add
+     * @param result binding
+     * @param model Trade to pass
+     * @return Trade adding page
+     */
     @PostMapping("/trade/validate")
-    public String validate(@Valid TradeEntity trade, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Trade list
+    public String validate(@Valid final TradeEntity trade,
+                           final BindingResult result, final Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("trade", trade);
+            //TODO display errors
+            return "trade/add";
+        }
+        tradeService.add(trade);
+        model.addAttribute("list", tradeService.findAll());
         return "trade/add";
     }
 
+    /**
+     * Go to updating Trade page.
+     * @param id Trade id
+     * @param model Trade to pass
+     * @return Trade updating page
+     */
     @GetMapping("/trade/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Trade by Id and to model then show to the form
+    public String showUpdateForm(@PathVariable("id") final Integer id,
+                                 final Model model) {
+        TradeEntity tradeEntity = tradeService.findById(id);
+        model.addAttribute("curvePoint", tradeEntity);
         return "trade/update";
     }
 
+    /**
+     * updating Trade page.
+     * @param id Trade id
+     * @param trade Trade to update
+     * @param result binding
+     * @param model list Trade
+     * @return Trade list page
+     */
     @PostMapping("/trade/update/{id}")
-    public String updateTrade(@PathVariable("id") Integer id, @Valid TradeEntity trade,
-                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Trade and return Trade list
+    public String updateTrade(@PathVariable("id") final Integer id,
+                              @Valid final TradeEntity trade,
+                             final BindingResult result, final Model model) {
+        if (result.hasErrors()) {
+            return "redirect:/trade/list";
+        }
+        trade.setTradeId(id);
+        tradeService.update(trade);
+        model.addAttribute("list", tradeService.findAll());
         return "redirect:/trade/list";
     }
 
+    /**
+     * Deleting Trade.
+     * @param id Trade id
+     * @param model list Trade
+     * @return Trade list page
+     */
     @GetMapping("/trade/delete/{id}")
-    public String deleteTrade(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Trade by Id and delete the Trade, return to Trade list
+    public String deleteTrade(@PathVariable("id") final Integer id,
+                              final Model model) {
+        tradeService.delete(id);
+        model.addAttribute("list", tradeService.findAll());
         return "redirect:/trade/list";
     }
 }
