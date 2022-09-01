12cb114227ed2329b796d7a673f30d081fe5182c
Myohres <77498044+Myohres@users.noreply.github.com>
2022-09-02 00:09:33 +0200

    RuleNameController and test ok steel @valid entity

--- src/main/java/com/nnk/poseidon/controllers/RuleNameController.java
+++ src/main/java/com/nnk/poseidon/controllers/RuleNameController.java
@@ -1,22 +1,20 @@
 package com.nnk.poseidon.controllers;
 
-import com.nnk.poseidon.domain.RatingEntity;
 import com.nnk.poseidon.domain.RuleNameEntity;
 import com.nnk.poseidon.service.RuleNameService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.NoSuchElementException;
 
-
+@Slf4j
 @Controller
 @CrossOrigin("http://localhost:4200")
 public class RuleNameController {
@@ -33,22 +31,18 @@ public class RuleNameController {
      * @return ruleName list page
      */
     @RequestMapping("/ruleName/list")
-    public String home(final Model model)
-    {
-        List<RuleNameEntity> list = ruleNameService.findAll();
-        model.addAttribute("list", list);
+    public String home(final Model model) {
+        model.addAttribute("list", ruleNameService.findAll());
         return "ruleName/list";
     }
 
     /**
      * Get adding ruleName page.
-     * @param ruleName ruleName to add
-     * @param model ruleName to pass
+     * @param ruleNameEntity ruleName to add
      * @return adding page
      */
     @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleNameEntity ruleName, final Model model) {
-        model.addAttribute("ruleName", ruleName);
+    public String addRuleForm(RuleNameEntity ruleNameEntity) {
         return "ruleName/add";
     }
 
@@ -60,15 +54,14 @@ public class RuleNameController {
      * @return ruleName list page
      */
     @PostMapping("/ruleName/validate")
-    public String validate(@Valid final RuleNameEntity ruleName, final BindingResult result, final Model model) {
+    public String validate(@Valid final RuleNameEntity ruleName,
+                           final BindingResult result, final Model model) {
         if (result.hasErrors()) {
-            model.addAttribute("ruleName", ruleName);
-            //TODO display errors
-        return "ruleName/add";
-        }
-            ruleNameService.add(ruleName);
-            model.addAttribute("list", ruleNameService.findAll());
             return "ruleName/add";
+        }
+        ruleNameService.add(ruleName);
+        model.addAttribute("list", ruleNameService.findAll());
+        return "redirect:/ruleName/list";
     }
 
     /**
@@ -78,9 +71,16 @@ public class RuleNameController {
      * @return ruleName update page
      */
     @GetMapping("/ruleName/update/{id}")
-    public String showUpdateForm(@PathVariable("id") final Integer id, final Model model) {
-        RuleNameEntity ruleNameEntity = ruleNameService.findById(id);
-        model.addAttribute("ruleName", ruleNameEntity);
+    public String showUpdateForm(@PathVariable("id") final Integer id,
+                                 final Model model) {
+       try {
+           RuleNameEntity ruleNameEntity = ruleNameService.findById(id);
+           model.addAttribute("ruleNameEntity", ruleNameEntity);
+       } catch (NoSuchElementException e) {
+           e.printStackTrace();
+           model.addAttribute("list", ruleNameService.findAll());
+           return "redirect:/ruleName/list";
+       }
         return "ruleName/update";
     }
 
@@ -93,7 +93,8 @@ public class RuleNameController {
      * @return ruleName list page if ok else ruleName update page
      */
     @PostMapping("/ruleName/update/{id}")
-    public String updateRuleName(@PathVariable("id") final Integer id, @Valid final RuleNameEntity ruleName,
+    public String updateRuleName(@PathVariable("id") final Integer id,
+                                 @Valid final RuleNameEntity ruleName,
                                  final BindingResult result, final Model model) {
         if (result.hasErrors()) {
             return "redirect:/ruleName/list";
@@ -111,9 +112,86 @@ public class RuleNameController {
      * @return ratting list page
      */
     @GetMapping("/ruleName/delete/{id}")
-    public String deleteRuleName(@PathVariable("id") final Integer id, final Model model) {
+    public String deleteRuleName(@PathVariable("id") final Integer id,
+                                 final Model model) {
         ruleNameService.delete(id);
         model.addAttribute("list", ruleNameService.findAll());
         return "redirect:/ruleName/list";
+    }
+
+    /**
+     * Get all ruleName.
+     * @return ruleName list
+     */
+    @GetMapping("/ruleName")
+    public ResponseEntity<List<RuleNameEntity>> getAllRuleName() {
+        log.info("GET/ruleName");
+        return ResponseEntity.ok(ruleNameService.findAll());
+    }
+
+    /**
+     * Get ruleName by id.
+     * @param ruleNameId id ruleName
+     * @return ruleName
+     */
+    @GetMapping("/ruleName/ruleNameId/{id}")
+    public ResponseEntity<RuleNameEntity> getRuleNameById(@PathVariable("id") final Integer ruleNameId) {
+        log.info("GET/ruleName/ruleNameId/" + ruleNameId);
+        try {
+            return ResponseEntity.ok(ruleNameService.findById(ruleNameId));
+        } catch (NoSuchElementException e) {
+            log.error("GetRuleNameById error : " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Add a ruleName.
+     * @param ruleNameEntity ruleName
+     * @return ruleName added
+     */
+    @PostMapping("/ruleName/add")
+    public ResponseEntity<RuleNameEntity> addRuleName(@RequestBody final RuleNameEntity ruleNameEntity) {
+        log.info("POST/ruleName/add");
+        try {
+            return ResponseEntity.ok(ruleNameService.add(ruleNameEntity));
+        } catch (NoSuchElementException e) {
+            log.error("addRuleName error : " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Update ruleName.
+     * @param ruleNameEntity ruleName new information
+     * @return ruleName updated
+     */
+    @PutMapping("/ruleName")
+    public ResponseEntity<RuleNameEntity> updateRuleName(
+            @RequestBody RuleNameEntity ruleNameEntity) {
+        log.info("PUT/ruleName/ruleNameId/ " + ruleNameEntity.getId());
+        try {
+            return ResponseEntity.ok(ruleNameService.update(ruleNameEntity));
+        } catch (NoSuchElementException e) {
+            log.error("UpdateRuleName error : " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Delete a ruleName by his id.
+     * @param ruleNameId id ruleName
+     * @return ruleName deleted
+     */
+    @DeleteMapping("/ruleName/ruleNameId/{id}")
+    public ResponseEntity<?> deleteRuleName(@PathVariable("id") final Integer ruleNameId) {
+        log.info("DEL/ruleName/ruleNameId/" + ruleNameId);
+        try {
+            ruleNameService.delete(ruleNameId);
+            return ResponseEntity.ok().build();
+        } catch (NoSuchElementException e) {
+            log.error("DeleteRuleName error " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/test/java/com/nnk/poseidon/controllers/RuleNameControllerTest.java
+++ src/test/java/com/nnk/poseidon/controllers/RuleNameControllerTest.java
@@ -4,20 +4,31 @@ import com.nnk.poseidon.domain.RuleNameEntity;
 import com.nnk.poseidon.service.RuleNameService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = RuleNameController.class)
-@AutoConfigureMockMvc(addFilters = false)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 class RuleNameControllerTest {
 
     @Autowired
@@ -26,51 +37,173 @@ class RuleNameControllerTest {
     @MockBean
     private RuleNameService ruleNameService;
 
-    RuleNameEntity ruleNameEntity;
+    RuleNameEntity ruleName;
 
     @BeforeEach
     void setUp() {
-        ruleNameEntity = new RuleNameEntity();
-        ruleNameEntity.setId(1);
-        ruleNameEntity.setName("name");
-        ruleNameEntity.setDescription("description");
-        ruleNameEntity.setJson("json");
-        ruleNameEntity.setTemplate("template");
-        ruleNameEntity.setSqlStr("sqlStr");
-        ruleNameEntity.setSqlPart("sqlPart");
+        ruleName = new RuleNameEntity();
+        ruleName.setId(1);
+        ruleName.setName("name");
+        ruleName.setDescription("description");
+        ruleName.setJson("json");
+        ruleName.setTemplate("template");
+        ruleName.setSqlStr("sqlStr");
+        ruleName.setSqlPart("sqlPart");
     }
 
     @Test
     void home() throws Exception {
-        when(ruleNameService.findAll()).thenReturn(new ArrayList<>());
-        mockMvc.perform(get("/ruleName/list")).andExpect(status().isOk());
+        mockMvc.perform(get("/ruleName/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("ruleName/list"))
+                .andExpect(model().attributeExists("list"));
     }
 
     @Test
     void addRuleNameForm() throws Exception {
-        when(ruleNameService.add(new RuleNameEntity())).thenReturn(new RuleNameEntity());
-        mockMvc.perform(get("/ruleName/add")).andExpect(status().isOk());
+        mockMvc.perform(get("/ruleName/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("ruleName/add"));
     }
 
     @Test
     void validate() throws Exception {
+        mockMvc.perform(post("/ruleName/validate")
+                .param("name","nn")
+                .param("description", "dd")
+                .param("json", "jj")
+                .param("template", "tt")
+                .param("sqlStr", "ss")
+                .param("sqlPart", "sp"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/ruleName/list"));
     }
 
     @Test
     void validateWithError() throws Exception {
+       /* mockMvc.perform(post("/ruleName/validate")
+                .param("name"," ")
+                .param("description", "dd")
+                .param("json", "jj")
+                .param("template", "tt")
+                .param("sqlStr", "ss")
+                .param("sqlPart", "sp"))
+                .andExpect(view().name("ruleName/add"));*/
     }
 
     @Test
     void showUpdateForm() throws Exception {
-        when(ruleNameService.findById(1)).thenReturn(ruleNameEntity);
-        mockMvc.perform(get("/ruleName/update/1")).andExpect(status().isOk());
+        when(ruleNameService.findById(1)).thenReturn(ruleName);
+        mockMvc.perform(get("/ruleName/update/1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("ruleNameEntity"))
+                .andExpect(view().name("ruleName/update"));
     }
 
     @Test
-    void updateRuleName() throws Exception {
+    void showUpdateFormWithNoRuleNameFound() throws Exception {
+        doThrow(new NoSuchElementException()).when(ruleNameService).findById(any());
+        mockMvc.perform(get("/ruleName/update/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/ruleName/list"));
+    }
+
+    @Test
+    void updateBidWithError() throws Exception {
+       /* mockMvc.perform(post("/ruleName/update/1")
+                .param("name","")
+                .param("description", "dd")
+                .param("json", "jj")
+                .param("template", "tt")
+                .param("sqlStr", "ss")
+                .param("sqlPart", "sp"))
+                .andExpect(view().name("ruleName/update"));*/
+    }
+
+    @Test
+    void updateRuleNameWithNoError() throws Exception {
+        when((ruleNameService.add(ruleName))).thenReturn(ruleName);
+        mockMvc.perform(post("/ruleName/update/1")
+                .param("account","aa")
+                .param("type", "tt")
+                .param("bidQuantity", "1d"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/ruleName/list"));
     }
 
     @Test
     void deleteRuleName() throws Exception {
+        when(ruleNameService.findById(any())).thenReturn(ruleName);
+        when(ruleNameService.findAll()).thenReturn(new ArrayList<>());
+        mockMvc.perform(get("/ruleName/delete/13"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/ruleName/list"));
     }
+
+    @Test
+    void getAllruleName() throws Exception {
+        when(ruleNameService.findAll()).thenReturn(new ArrayList<>());
+        mockMvc.perform(get("/ruleName"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getRuleNameById() throws Exception {
+        when(ruleNameService.findById(any())).thenReturn(ruleName);
+        mockMvc.perform(get("/ruleName/ruleNameId/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getRuleNameByIdNotFound() throws Exception {
+        when(ruleNameService.findById(any())).thenThrow(new NoSuchElementException());
+        mockMvc.perform(get("/ruleName/ruleNameId/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void addRuleName() throws Exception {
+        mockMvc.perform(post("/ruleName/add")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void addRuleNameError() throws Exception {
+        when(ruleNameService.add(any())).thenThrow(new NoSuchElementException());
+        mockMvc.perform(post("/ruleName/add")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void updateRuleName() throws Exception {
+        when(ruleNameService.update(any())).thenReturn(ruleName);
+        mockMvc.perform(put("/ruleName")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateRuleNameNotFound() throws Exception {
+        when(ruleNameService.update(any())).thenThrow(new NoSuchElementException());
+        mockMvc.perform(put("/ruleName")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testDeleteRuleName() throws Exception {
+        when(ruleNameService.findById(any())).thenReturn(ruleName);
+        mockMvc.perform(delete("/ruleName/ruleNameId/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteRuleNameNotFound() throws Exception {
+        doThrow(new NoSuchElementException()).when(ruleNameService).delete(1);
+        mockMvc.perform(delete("/ruleName/ruleNameId/1"))
+                .andExpect(status().isNotFound());
+    }
+
 }
