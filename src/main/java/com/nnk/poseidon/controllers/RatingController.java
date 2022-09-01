492ed5e680b5499921ffb86b879eba1efa2e0e9b
Myohres <77498044+Myohres@users.noreply.github.com>
2022-09-01 23:35:43 +0200

    RatingController and test ok steel @valid entity

--- src/main/java/com/nnk/poseidon/controllers/RatingController.java
+++ src/main/java/com/nnk/poseidon/controllers/RatingController.java
@@ -2,19 +2,19 @@ package com.nnk.poseidon.controllers;
 
 import com.nnk.poseidon.domain.RatingEntity;
 import com.nnk.poseidon.service.RatingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+@Slf4j
 @Controller
 @CrossOrigin("http://localhost:4200")
 public class RatingController {
@@ -32,20 +32,17 @@ public class RatingController {
      */
     @RequestMapping("/rating/list")
     public String home(final Model model) {
-        List<RatingEntity> list = ratingService.findAll();
-        model.addAttribute("list", list);
+        model.addAttribute("list", ratingService.findAll());
         return "rating/list";
     }
 
     /**
      * Get adding ratting page.
-     * @param rating ratting to add
-     * @param model ratting to pass
+     * @param ratingEntity ratting to add
      * @return adding page
      */
     @GetMapping("/rating/add")
-    public String addRatingForm(final RatingEntity rating, final Model model) {
-        model.addAttribute("rating", rating);
+    public String addRatingForm(final RatingEntity ratingEntity) {
         return "rating/add";
     }
 
@@ -60,13 +57,11 @@ public class RatingController {
     public String validate(@Valid final RatingEntity rating,
                            final BindingResult result, final Model model) {
         if (result.hasErrors()) {
-            model.addAttribute("rating", rating);
-            //TODO display errors
             return "rating/add";
         }
         ratingService.add(rating);
         model.addAttribute("list", ratingService.findAll());
-        return "rating/list";
+        return "redirect:/rating/list";
     }
 
     /**
@@ -78,8 +73,14 @@ public class RatingController {
     @GetMapping("/rating/update/{id}")
     public String showUpdateForm(@PathVariable("id") final Integer id,
                                  final Model model) {
-        RatingEntity ratingEntity = ratingService.findById(id);
-        model.addAttribute("rating", ratingEntity);
+        try {
+            RatingEntity ratingEntity = ratingService.findById(id);
+            model.addAttribute("rating", ratingEntity);
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            model.addAttribute("list", ratingService.findAll());
+            return "redirect:/rating/list";
+        }
         return "rating/update";
     }
 
@@ -116,5 +117,81 @@ public class RatingController {
         ratingService.delete(id);
         model.addAttribute("list", ratingService.findAll());
         return "redirect:/rating/list";
+    }
+
+    /**
+     * Get all rating.
+     * @return rating list
+     */
+    @GetMapping("/rating")
+    public ResponseEntity<List<RatingEntity>> getAllRating() {
+        log.info("GET/rating");
+        return ResponseEntity.ok(ratingService.findAll());
+    }
+
+    /**
+     * Get rating by id.
+     * @param ratingId id rating
+     * @return rating
+     */
+    @GetMapping("/rating/ratingId/{id}")
+    public ResponseEntity<RatingEntity> getRatingById(@PathVariable("id") final Integer ratingId) {
+        log.info("GET/rating/ratingId/" + ratingId);
+        try {
+            return ResponseEntity.ok(ratingService.findById(ratingId));
+        } catch (NoSuchElementException e) {
+            log.error("GetRatingById error : " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Add a rating.
+     * @param ratingEntity rating
+     * @return rating added
+     */
+    @PostMapping("/rating/add")
+    public ResponseEntity<RatingEntity> addRating(@RequestBody final RatingEntity ratingEntity) {
+        log.info("POST/rating/add");
+        try {
+            return ResponseEntity.ok(ratingService.add(ratingEntity));
+        } catch (NoSuchElementException e) {
+            log.error("addRating error : " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Update rating.
+     * @param ratingEntity rating new information
+     * @return rating updated
+     */
+    @PutMapping("/rating")
+    public ResponseEntity<RatingEntity> updateRating(
+            @RequestBody RatingEntity ratingEntity) {
+        log.info("PUT/rating/ratingId/ " + ratingEntity.getId());
+        try {
+            return ResponseEntity.ok(ratingService.update(ratingEntity));
+        } catch (NoSuchElementException e) {
+            log.error("UpdateRating error : " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Delete a rating by his id.
+     * @param ratingId id rating
+     * @return rating deleted
+     */
+    @DeleteMapping("/rating/ratingId/{id}")
+    public ResponseEntity<?> deleteRating(@PathVariable("id") final Integer ratingId) {
+        log.info("DEL/rating/ratingId/" + ratingId);
+        try {
+            ratingService.delete(ratingId);
+            return ResponseEntity.ok().build();
+        } catch (NoSuchElementException e) {
+            log.error("DeleteRating error " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/test/java/com/nnk/poseidon/controllers/RatingControllerTest.java
+++ src/test/java/com/nnk/poseidon/controllers/RatingControllerTest.java
@@ -4,71 +4,209 @@ import com.nnk.poseidon.domain.RatingEntity;
 import com.nnk.poseidon.service.RatingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = RatingController.class)
-@AutoConfigureMockMvc(addFilters = false)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 class RatingControllerTest {
 
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mockMvc;
 
     @MockBean
     private RatingService ratingService;
 
-    RatingEntity ratingEntity;
+    RatingEntity rating;
 
     @BeforeEach
     void setUp() {
-        ratingEntity = new RatingEntity();
-        ratingEntity.setId(1);
-        ratingEntity.setMoodysRating("moody");
-        ratingEntity.setFitchRating("fitch");
-        ratingEntity.setSandPRating("sandP");
-        ratingEntity.setOrderNumber(1);
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+
+        rating = new RatingEntity();
+        rating.setId(1);
+        rating.setMoodysRating("moody");
+        rating.setFitchRating("fitch");
+        rating.setSandPRating("sandP");
+        rating.setOrderNumber(1);
     }
 
     @Test
     void home() throws Exception {
-        when(ratingService.findAll()).thenReturn(new ArrayList<>());
-        mockMvc.perform(get("/rating/list")).andExpect(status().isOk());
+        mockMvc.perform(get("/rating/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("rating/list"))
+                .andExpect(model().attributeExists("list"));
     }
 
     @Test
     void addRatingForm() throws Exception {
-        when(ratingService.add(new RatingEntity())).thenReturn(new RatingEntity());
-        mockMvc.perform(get("/rating/add")).andExpect(status().isOk());
+        mockMvc.perform(get("/rating/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("rating/add"));
     }
 
     @Test
     void validate() throws Exception {
+        mockMvc.perform(post("/rating/validate")
+                .param("moodysRating","mm")
+                .param("sandPRating", "ss")
+                .param("fitchRating", "ff")
+                .param("orderNumber","1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/rating/list"));
     }
 
     @Test
     void validateWithError() throws Exception {
+       /* mockMvc.perform(post("/rating/validate")
+                .param("moodysRating","mm")
+                .param("sandPRating", "ss")
+                .param("fitchRating", "ff")
+                .param("orderNumber","1"))
+                .andExpect(view().name("rating/add"));*/
     }
 
     @Test
     void showUpdateForm() throws Exception {
-        when(ratingService.findById(1)).thenReturn(ratingEntity);
-        mockMvc.perform(get("/rating/update/1")).andExpect(status().isOk());
+        when(ratingService.findById(1)).thenReturn(rating);
+        mockMvc.perform(get("/rating/update/1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("rating"))
+                .andExpect(view().name("rating/update"));
+    }
+
+    @Test
+    void showUpdateFormWithNoRatingFound() throws Exception {
+        doThrow(new NoSuchElementException()).when(ratingService).findById(any());
+        mockMvc.perform(get("/rating/update/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/rating/list"));
     }
 
     @Test
     void updateRating() throws Exception {
+        when((ratingService.add(rating))).thenReturn(rating);
+        mockMvc.perform(post("/rating/update/1")
+                .param("moodysRating","mm")
+                .param("sandPRating", "ss")
+                .param("fitchRating", "ff")
+                .param("orderNumber","1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/rating/list"));
+    }
+
+    @Test
+    void updateRatingWithError() throws Exception {
+       /* mockMvc.perform(post("/rating/update/1")
+                .param("moodysRating"," ")
+                .param("sandPRating", "ss")
+                .param("fitchRating", "ff")
+                .param("orderNumber","1"))
+                .andExpect(view().name("rating/update"));*/
     }
 
     @Test
     void deleteRating() throws Exception {
+        when(ratingService.findById(any())).thenReturn(rating);
+        when(ratingService.findAll()).thenReturn(new ArrayList<>());
+        mockMvc.perform(get("/rating/delete/13"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/rating/list"));
+    }
+
+
+    @Test
+    void getAllRating() throws Exception {
+        when(ratingService.findAll()).thenReturn(new ArrayList<>());
+        mockMvc.perform(get("/rating"))
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    void getRatingById() throws Exception {
+        when(ratingService.findById(any())).thenReturn(rating);
+        mockMvc.perform(get("/rating/ratingId/1"))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void getRatingByIdNotFound() throws Exception {
+        when(ratingService.findById(any())).thenThrow(new NoSuchElementException());
+        mockMvc.perform(get("/rating/ratingId/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void addRating() throws Exception {
+        mockMvc.perform(post("/rating/add")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void addRatingError() throws Exception {
+        when(ratingService.add(any())).thenThrow(new NoSuchElementException());
+        mockMvc.perform(post("/rating/add")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void updateRating2() throws Exception {
+        when(ratingService.update(any())).thenReturn(rating);
+        mockMvc.perform(put("/rating")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateRatingNotFound() throws Exception {
+        when(ratingService.update(any())).thenThrow(new NoSuchElementException());
+        mockMvc.perform(put("/rating")
+                .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testDeleteRating() throws Exception {
+        mockMvc.perform(delete("/rating/ratingId/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteRatingNotFound() throws Exception {
+        doThrow(new NoSuchElementException()).when(ratingService).delete(1);
+        mockMvc.perform(delete("/rating/ratingId/1"))
+                .andExpect(status().isNotFound());
     }
 }
