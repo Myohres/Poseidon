ff04407a5a6bad798ad16dbd6b34802a7a3f7bf5
Myohres <77498044+Myohres@users.noreply.github.com>
2022-09-13 12:15:21 +0200

    security formlogin, logout and oauth2

--- src/main/java/com/nnk/poseidon/security/SecurityConfig.java
+++ src/main/java/com/nnk/poseidon/security/SecurityConfig.java
@@ -1,30 +1,51 @@
 package com.nnk.poseidon.security;
 
+import com.nnk.poseidon.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserService userService;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(("/")).permitAll()
+                .antMatchers("/login*").permitAll()
+                .antMatchers(("/user/**")).hasRole("ADMIN")
                 .anyRequest().authenticated()
-                .and().formLogin();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("simon").password(new BCryptPasswordEncoder().encode("root")).roles("ADMIN");
+                .and().formLogin()
+                    .loginProcessingUrl("/perform_login")
+                    .defaultSuccessUrl("/bidList/list",false)
+                    .failureUrl("/login.html?error=true")
+                .and().oauth2Login()
+                   /* .loginProcessingUrl("/perform_login")
+                   /* .defaultSuccessUrl("/bidList/list",false)*/
+                .and().logout()
+                   .logoutUrl("/app-logout")
+                    .logoutSuccessUrl("/login")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+                    ;
     }
 
     @Bean
