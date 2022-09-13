62f81257cc9b3952356dd941b5ecbb4d7c3e9fd8
Myohres <77498044+Myohres@users.noreply.github.com>
2022-09-13 12:19:48 +0200

    security

--- src/main/java/com/nnk/poseidon/PoseidonApplication.java
+++ src/main/java/com/nnk/poseidon/PoseidonApplication.java
@@ -1,13 +1,27 @@
 package com.nnk.poseidon;
 
+import com.nnk.poseidon.domain.UserEntity;
+import com.nnk.poseidon.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class PoseidonApplication {
+public class PoseidonApplication implements CommandLineRunner {
+
+   /* @Autowired
+    UserRepository userRepository;*/
+
+   /* @Autowired
+    PasswordEncoder passwordEncoder;*/
 
     public static void main(String[] args) {
         SpringApplication.run(PoseidonApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+    }
 }
