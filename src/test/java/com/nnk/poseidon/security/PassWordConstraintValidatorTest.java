f8adb1d16ce774e0a4e0f83a234788e65e9ca176
Myohres <77498044+Myohres@users.noreply.github.com>
2022-09-18 02:00:59 +0200

    fix lengtRule 30->80

---
+++ src/test/java/com/nnk/poseidon/security/PassWordConstraintValidatorTest.java
@@ -0,0 +1,52 @@
+package com.nnk.poseidon.security;
+
+import com.nnk.poseidon.domain.UserEntity;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class PassWordConstraintValidatorTest {
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void testInvalidPassword() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+        userEntity.setUsername("userName");
+        userEntity.setFullname("fullName");
+        userEntity.setPassword("password");
+        userEntity.setRole("role");
+
+        Set<ConstraintViolation<UserEntity>> constraintViolations = validator.validate(userEntity);
+        Assert.assertEquals(constraintViolations.size(),1);
+    }
+
+    @Test
+    public void testValidPassword() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+        userEntity.setUsername("userName");
+        userEntity.setFullname("fullName");
+        userEntity.setPassword("password8A@");
+        userEntity.setRole("role");
+
+        Set<ConstraintViolation<UserEntity>> constraintViolations = validator.validate(userEntity);
+        Assert.assertEquals(constraintViolations.size(),0);
+    }
+
+}
